acf9e0f80dfe495d85d9e82fff9fba2d06b1ea10
IshratEpsi16 <ishratepsi16@gmail.com>
2022-01-31 22:11:33 +0600

    update

--- App_Login/models.py
+++ App_Login/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # to create custom user model and admin panel
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import gettext_lazy
 #to automatically create one to one objects
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -33,14 +33,14 @@ class MyUserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True,null=False)
     is_staff = models.BooleanField(
-        ugettext_lazy('staff status'),
+        gettext_lazy('staff status'),
         default=True,
-        help_text=ugettext_lazy('Designates whether the user can log in this site')
+        help_text=gettext_lazy('Designates whether the user can log in this site')
     )
     is_active = models.BooleanField(
-        ugettext_lazy('active'),
+        gettext_lazy('active'),
         default=True,
-        help_text=ugettext_lazy('Designates whether this user should be treated as active. Unselect this instead of deleting accounts')
+        help_text=gettext_lazy('Designates whether this user should be treated as active. Unselect this instead of deleting accounts')
         
     )
     USERNAME_FIELD = 'email'
@@ -66,8 +66,8 @@ class Profile(models.Model):
     def __str__(self):
         return self.user + "'s Profile"
     def is_fully_filled(self):
-        fields_name = [f.name for f in self._meta.get_fields()]
-        for field_name in fields_name:
+        fields_names = [f.name for f in self._meta.get_fields()]
+        for field_name in fields_names:
             value = getattr(self, field_name)
             if value is None or value==' ':
                 return False
